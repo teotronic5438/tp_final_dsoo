815314e6ddc196d23301f6eae897a31f15938c28
Max-Power1 <ramiro01032003@gmail.com>
2024-06-27 18:13:34 -0300

    nombrando metodos de clases

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -1,1 +1,31 @@
-print("hello world!")
+from abc import ABC, abstractmethod
+from abc import ABCMeta
+
+class GestionarObra(metaclass=ABCMeta):
+    @abstractmethod
+    def extraer_datos(self):
+        pass
+
+    @abstractmethod
+    def conectar_db(self):
+        pass
+
+    @abstractmethod
+    def mapear_orm(self):
+        pass
+
+    @abstractmethod
+    def limpiar_datos(self):
+        pass
+
+    @abstractmethod
+    def cargar_datos(self):
+        pass
+
+    @abstractmethod
+    def nueva_obra(self):
+        pass
+
+    @abstractmethod
+    def obtener_indicadores(self):
+        pass
