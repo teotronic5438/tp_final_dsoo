8240839dbae1031502a3620ab16b8d7e66a8d689
Max-Power1 <ramiro01032003@gmail.com>
2024-06-28 18:10:08 -0300

    conectar a la base de datos

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -1,15 +1,24 @@
 from abc import ABC, abstractmethod
 from abc import ABCMeta
-import pandas as pd
+#import pandas as pd
+import sqlite3
 
 class GestionarObra(metaclass=ABCMeta):
     @abstractmethod
     def extraer_datos(self):
-        df = pd.read_csv('observatorio-de-obras-urbanas.csv', header= None)
+        #df = pd.read_csv('observatorio-de-obras-urbanas.csv', header= None)
+        pass
 
     @abstractmethod
     def conectar_db(self):
-        pass
+        #una idea de como creo que se conecta a la base de datos
+        try:
+            conn = sqlite3.connect("obras_urbanas.db")
+        except:
+            print("No se pudo conectar a la base")
+        else:
+            print("Se conecto correctamente a la base")
+        return conn
 
     @abstractmethod
     def mapear_orm(self):
